aa10fc9c64b5ab34f97cebd509cdcbe59226e70d
Rafael Gábrio <rafael-sims@live.com>
2021-11-24 12:16:44 -0300

    criado novos componentes e novas estilizações

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
+
+import Container from './components/layout/Container';
+
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/company">Companhia</Link>
+        <Link to="/newproject">Novo Projeto</Link>
+      </div>
+
+      <Container customClass="min-height">
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route path="/company" element={<Company />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+          <Route path="/newproject" element={<NewProject />}></Route>
+        </Routes>
+      </Container>
+      <p>Footer</p>
+    </Router>
   );
 }
 
