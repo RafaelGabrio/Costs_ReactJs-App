1413e89d7254dda0a8a52cdf5f0f768163b3adc3
Rafael Gábrio <rafael-sims@live.com>
2021-12-01 15:45:08 -0300

    substituido a tipagem dos arquivos .js por .jsx

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,16 @@
+import styles from './Home.module.css';
+import savings from '../../img/savings.svg';
+import LinkButton from '../layout/LinkButton';
+
+function Home() {
+    return (
+        <section className={styles.home_container}>
+            <h1>Bem-vindo ao <span>Costs</span></h1>
+            <p>Comece a gerenciar os seus projetos agora mesmo!</p>
+            <LinkButton to="/newproject" text="Criar Projeto" />
+            <img src={savings} alt="Coscts" />
+        </section>
+    );
+}
+
+export default Home;
