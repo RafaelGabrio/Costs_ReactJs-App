480efa6706ac709ab541cb6f34b78f07a774e323
Rafael Gábrio <rafael-sims@live.com>
2022-03-31 18:01:51 -0300

    adição de estilização e outros componentes na página de projetos

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,19 +1,29 @@
 import { useLocation } from "react-router-dom";
 
+import Container from "../layout/Container";
+import LinkButton from "../layout/LinkButton";
 import Message from "../layout/Message";
+
+import styles from "./Projects.module.css";
 
 function Projects() {
 
     const location = useLocation();
     let message = '';
-    if(location.state) {
+    if (location.state) {
         message = location.state.message;
     }
 
     return (
-        <div>
-            <h1>Meus Projetos</h1>
-            {message && <Message msg={message} type={"succes"}/>}
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to={"/newproject"} text="Criar Projeto" />
+            </div>
+            {message && <Message msg={message} type={"succes"} />}
+            <Container customClass="start">
+                <p>Projetos...</p>
+            </Container>
         </div>
     )
 }
